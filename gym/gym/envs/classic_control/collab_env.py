9756d00505c97e0f184ba0b16b354829f5db341b
H <hedi.hadiji@gmail.com>
2017-01-14 17:49:29 +0100

    Normalement ça devrait être bon

--- gym/gym/envs/classic_control/collab_env.py
+++ gym/gym/envs/classic_control/collab_env.py
@@ -12,95 +12,97 @@ import numpy as np
 
 logger = logging.getLogger(__name__)
 
-min_x, min_y, min_vx, min_vy = -5., 0., -15., -15.
-max_x, max_y, max_vx, max_vy = 5., 10., 15., 15.
+min_x, min_y, min_vx, min_vy = -5., 0., -5., -15.
+max_x, max_y, max_vx, max_vy = 5., 8., 5., 15.
 
 
 class Agent(object):
 
-	def __init__(self, x, y, length, height):
-		self.gravity = 1.
+	def __init__(self, x=0., y=0.5, length=0.5, height=0.5, agent_number=0):
+		self.gravity = 4.
 		self.mass = 1.0
 		self.height = height # half height
 		self.length = length # half length
+		self.agent_number = agent_number
 
 		self.min_x, self.min_y, self.min_vx, self.min_vy = min_x + self.length, min_y + self.height, min_vx, min_vy
 		self.max_x, self.max_y, self.max_vx, self.max_vy = max_x - self.length, max_y - self.height, max_vx, max_vy
 
-		self.on_firm_ground = True # = can_jump
+		self.on_firm_ground = True # = can jump
 
-		self.jump_force = 10.0
-		self.force_mag = 2.0
-		self.tau = 0.2
+		self.jump_force = 80.0
+		self.force_mag = 10.0
+		self.tau = 0.05
 
 		self.x = x
 		self.y = y
 		self.vx = 0.
 		self.vy = 0.
 
+
 	def up_down_update(self, agents, obstacles, jump=False): # handles gravity and up/down movement collisions
 
 		if not(self.on_firm_ground):
 			new_vy = max(self.vy - self.gravity * self.tau, self.min_vy)
 		if self.on_firm_ground:
+			print str(self.agent_number) + " is on firm ground"
 			if jump:
-				new_vy = min(self.vy + (self.jump_force - self.gravity) * self.tau / self.mass, self.max_vy)
+				new_vy = min(self.vy + (self.jump_force / self.mass - self.gravity) * self.tau , self.max_vy)
 				self.on_firm_ground = False
 			else:
 				new_vy = 0.
 
 		new_y = self.y + new_vy * self.tau
 
-
-		current_max = -1.
+		current_max = -1000.
 		current_min = 1000.
 		intersect_agent_upwards = False
 		intersect_agent_downwards = False
+		contact = False
 		for agent in agents:
-			if (new_y < agent.y + self.height + agent.height) and (new_y > agent.y) and (abs(self.x - agent.x) < self.length + agent.length):
-				current_max = max(current_max, agent.y + self.height + agent.height)
-				intersect_agent_downwards = True
-			if (new_y > agent.y - self.height - agent.height) and (new_y < agent.y) and (abs(self.x - agent.x) < self.length + agent.length):
-				current_min  = min(current_min, agent.y - self.height - agent.height)
-				intersect_agent_upwards = True
+			if not(self.agent_number == agent.agent_number):
+				if (new_y < agent.y + self.height + agent.height) and (self.y > agent.y) and (abs(self.x - agent.x) < self.length + agent.length):
+					current_max = max(current_max, agent.y + self.height + agent.height)
+					intersect_agent_downwards = True
+				if (new_y > agent.y - self.height - agent.height) and (self.y < agent.y) and (abs(self.x - agent.x) < self.length + agent.length):
+					current_min  = min(current_min, agent.y - self.height - agent.height)
+					intersect_agent_upwards = True
+				if (abs(self.y - agent.y) <= self.height + agent.height) and (abs(self.x - agent.x) < (self.length + agent.length)):
+					contact = True
 
-		if intersect_agent_downwards:
-			self.vy = 0.
-			self.y = current_max
-			self.on_firm_ground = True
-		if intersect_agent_upwards:
-			self.vy = 0.
-			self.y = current_min
+		if not(contact):
 			self.on_firm_ground = False
 
-		current_max = current_max
-		current_min = current_min
 		intersect_obstacle_upwards = False
 		intersect_obstacle_downwards = False
 
 		for obstacle in obstacles: 
-			if (new_y < obstacle.y + self.height + obstacle.height) and (new_y > obstacle.y) and (abs(self.x - obstacle.x) < self.length + obstacle.length):
+			if (new_y < obstacle.y + self.height + obstacle.height) and (self.y > obstacle.y) and (abs(self.x - obstacle.x) < self.length + obstacle.length):
 				current_max = max(current_max, obstacle.y + self.height + obstacle.height)
 				intersect_obstacle_downwards = True
-			if (new_y > obstacle.y - self.height - obstacle.height) and (new_y < obstacle.y) and (abs(self.x - obstacle.x) < self.length + obstacle.length):
+			if (new_y > obstacle.y - self.height - obstacle.height) and (self.y < obstacle.y) and (abs(self.x - obstacle.x) < self.length + obstacle.length):
 				current_min = min(current_min, obstacle.y - self.height - obstacle.height)
 				intersect_obstacle_upwards = True
 
-		if intersect_obstacle_downwards:
+		intersect_downwards = intersect_obstacle_downwards or intersect_agent_downwards 
+		intersect_upwards = intersect_agent_upwards or intersect_obstacle_upwards
+
+		if intersect_downwards:
 			self.vy = 0.
 			self.y = current_max
 			self.on_firm_ground = True
-		if intersect_obstacle_upwards:
+		if intersect_upwards:
 			self.vy = 0.
 			self.y = current_min
 
-		intersect = intersect_obstacle_downwards or intersect_agent_downwards or intersect_agent_upwards or intersect_obstacle_upwards
+		intersect = intersect_downwards or intersect_upwards
+		
 		if not(intersect):
-			if new_y < self.min_y:
+			if new_y <= self.min_y:
 				self.vy = 0.
-				self.y = self.height
+				self.y = self.min_y
 				self.on_firm_ground = True
-			elif new_y > self.max_y:
+			elif new_y >= self.max_y:
 				self.vy = 0.
 				self.y = self.max_y
 			else:
@@ -109,23 +111,22 @@ class Agent(object):
 
 		return
 
-	def left_right_update(self, obstacles, agents, dir=0):
-		new_vx = min( max(self.vx + dir * self.force_mag / self.mass * self.tau, self.min_vx), self.max_vx)
+	def left_right_update(self, agents, obstacles, direction=0):
+		new_vx = min( max(self.vx + direction * self.force_mag / self.mass * self.tau, self.min_vx), self.max_vx)
 		new_x = self.x + new_vx * self.tau
 
-		#new_x = min(max(new_x, self.min_x), self.max_x)
-
-		current_max = -1.
+		current_max = -1000.
 		current_min = 1000.
 		intersect_agent_leftwards = False
 		intersect_agent_rightwards = False
 		for agent in agents:
-			if (new_x < (agent.x + self.length + agent.length)) and (self.x >= agent.x) and (abs(self.y - agent.y) < (self.height + agent.height)):
-				current_max = max(current_max, agent.x + self.length + agent.length)
-				intersect_agent_leftwards = True
-			if (new_x > (agent.x - self.length - agent.length)) and (self.x <= agent.x) and (abs(self.y - agent.y) < (self.height + agent.height)):
-				current_min = min(current_min, agent.x - self.length - agent.length)
-				intersect_agent_rightwards = True
+			if not(agent.agent_number == self.agent_number):
+				if (new_x < (agent.x + self.length + agent.length)) and (self.x >= agent.x) and (abs(self.y - agent.y) < (self.height + agent.height)):
+					current_max = max(current_max, agent.x + self.length + agent.length)
+					intersect_agent_leftwards = True
+				if (new_x > (agent.x - self.length - agent.length)) and (self.x <= agent.x) and (abs(self.y - agent.y) < (self.height + agent.height)):
+					current_min = min(current_min, agent.x - self.length - agent.length)
+					intersect_agent_rightwards = True
 
 		intersect_obstacle_leftwards = False
 		intersect_obstacle_rightwards = False
@@ -135,7 +136,6 @@ class Agent(object):
 				intersect_obstacle_leftwards = True
 			if (new_x > (obstacle.x - self.length - obstacle.length)) and (self.x <= obstacle.x) and (abs(self.y - obstacle.y) < (self.height + obstacle.height)):
 				current_min = min(current_min, obstacle.x - self.length - obstacle.length)
-				print 
 				intersect_obstacle_rightwards = True
 
 		intersect_leftwards = intersect_obstacle_leftwards or intersect_agent_leftwards
@@ -164,19 +164,19 @@ class Agent(object):
 		return
 
 	def wait(self, agents, obstacles):
-		self.left_right_update(agents, obstacles, dir=0)
+		self.left_right_update(agents, obstacles, direction=0)
 		self.up_down_update(agents, obstacles, jump=False)
 
 	def jump(self, agents, obstacles):
-		self.left_right_update(agents, obstacles, dir=0)
+		self.left_right_update(agents, obstacles, direction=0)
 		self.up_down_update(agents, obstacles, jump=True)
 
-	def move_left(self, obstacles, agents):
-		self.left_right_update(agents, obstacles, dir=-1)
+	def move_left(self, agents, obstacles):
+		self.left_right_update(agents, obstacles, direction=-1)
 		self.up_down_update(agents, obstacles, jump=False)
 
-	def move_right(self, obstacles, agents):
-		self.left_right_update(agents, obstacles, dir=1)
+	def move_right(self, agents, obstacles):
+		self.left_right_update(agents, obstacles, direction=1)
 		self.up_down_update(agents, obstacles, jump=False)
 
 class Obstacle(object):
@@ -189,28 +189,29 @@ class Obstacle(object):
 
 class Star(object):
 
-	def __init__(self, x, y):
-		self.x = 1.5
-		self.y = 1.
-		self.length = 1. 
-		self.height = 1.
+	def __init__(self, x, y, length, height):
+		self.x = x
+		self.y = y
+		self.length = length 
+		self.height = height
+		self.found = False
 
 class CollabEnv(gym.Env):
 	metadata = {
         'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second' : 60
+        'video.frames_per_second' : 1
     }
 
 	def __init__(self):
 		self.gravity = 9.8
 		self.tau = 0.02  # seconds between state updates
-		self.number_of_agents = 5
+		self.number_of_agents = 2
 		self.viewer = None
 
 		self.agents = []
 		self.obstacles = []
 
-		self.star = None
+		self.star = None 
 
 		self.min_x, self.min_y, self.min_vx, self.min_vy = min_x, min_y, min_vx, min_vy
 		self.max_x, self.max_y, self.max_vx, self.max_vy = max_x, max_y, max_vx, max_vy
@@ -226,7 +227,6 @@ class CollabEnv(gym.Env):
 		self.observation_space = spaces.Box(self.low, self.high)
 
 		self._seed()
-		
 
 		#self.steps_beyond_done = None
 		#self._reset()
@@ -243,12 +243,21 @@ class CollabEnv(gym.Env):
 			reward += agent.y
 
 			star = self.star
-			if (abs(agent.x - star.x) < (agent.length + star.length) ) and (abs(agent.y - star.y) < (agent.height + star.height)):
-				reward += 1000.
+			if not(star.found) and (abs(agent.x - star.x) < (agent.length + star.length) ) and (abs(agent.y - star.y) < (agent.height + star.height)):
+				reward += 100.
+				star.found = True
 				done = True
 
 		reward = reward / self.number_of_agents
 		return reward, done
+
+	def others_wait(self, agent_number):
+		for agent in self.agents:
+			if not(agent.agent_number == agent_number):
+				agent.wait(self.agents, self.obstacles)
+		return
+		
+
 
 	def _step(self, action):
 		assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
@@ -256,20 +265,17 @@ class CollabEnv(gym.Env):
 		agent_number = action / 4
 		to_do = action % 4
 		
-		other_agents = [a for a in self.agents]
-		del other_agents[agent_number]
-
-		other_agents = np.array(other_agents)
 
 		if to_do == 0:
-			self.agents[agent_number].move_left(other_agents, self.obstacles)
+			self.agents[agent_number].move_left(self.agents, self.obstacles)
 		if to_do == 1:
-			self.agents[agent_number].wait(other_agents, self.obstacles)
+			self.agents[agent_number].wait(self.agents, self.obstacles)
 		if to_do == 2:
-			self.agents[agent_number].move_right(other_agents, self.obstacles)
+			self.agents[agent_number].move_right(self.agents, self.obstacles)
 		if to_do == 3:
-			self.agents[agent_number].jump(other_agents, self.obstacles)
+			self.agents[agent_number].jump(self.agents, self.obstacles)
 
+		self.others_wait(agent_number)
 		reward, done = self.update()
 
 		return np.array(self.state), reward, done, {}
@@ -282,17 +288,20 @@ class CollabEnv(gym.Env):
 	def _reset(self): # pas pensé pour trop d'agents
 
 		self.state = np.zeros(4 * self.number_of_agents)
+		self.agents = []
+		self.obstacles = []
 
 		for i in range(self.number_of_agents):
-			self.agents.append(Agent(self.min_x + i, self.min_y, 0.5, 0.5))
+			self.agents.append(Agent(x=self.min_x + 1., y=0.5 + 3*i, length=0.5, height=0.5, agent_number=i))
 
-		for i, agent in enumerate(self.agents):
+		for agent in self.agents:
+			i = agent.agent_number
 			self.state[4*i] = agent.x
 			self.state[4*i + 1] = agent.y
 			self.state[4*i + 2] = agent.vx
 			self.state[4*i + 3] = agent.vy
 	    
-		self.star = Star(2., 2.)
+		self.star = Star(2., 0.5, 0.5, 0.5)
 		self.obstacles = [Obstacle(0., 1., 0.5, 2.)]
 
 		return np.array(self.state)
@@ -321,7 +330,8 @@ class CollabEnv(gym.Env):
 		    self.agents_render = []
 		    self.agents_trans = []
 
-		    for i, agent in enumerate(self.agents):
+		    for agent in self.agents:
+		    	i = agent.agent_number
 		    	l,r,t,b =  -agent.length, agent.length, agent.height, -agent.height
 		    	l,r,t,b = l*x_scale, r*x_scale, t*y_scale, b*y_scale
 		    	agent_image = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
@@ -367,9 +377,9 @@ class CollabEnv(gym.Env):
 			y = (y - self.min_y)*y_scale
 			self.obstacles_trans[i].set_translation(x, y)
 
-		for i, agent in enumerate(self.agents):
+		for agent in self.agents:
+			i = agent.agent_number
 			x, y = agent.x, agent.y
-			print x, y
 			x = (x - self.min_x)*x_scale
 			y = (y - self.min_y)*y_scale
 			self.agents_trans[i].set_translation(x, y)
